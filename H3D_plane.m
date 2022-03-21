87605465d86ba1ba955db306aaf8987804b9bbe5
pablito <pawcio27610@wp.pl>
2022-03-21 11:51:26 +0100

    start fn od hough3d

--- H3D_plane.m
+++ H3D_plane.m
@@ -19,4 +19,22 @@ axis equal
 xlim([0 sz(1)])
 ylim([0 sz(2)])
 zlim([0 sz(3)])
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+[H, Theta, Phi, Rho] = hough3Dplane(data);
+
+
+function[h, theta, phi, rho] = hough3Dplane(BW)
+theta=-90:89;
+phi=-90:89;
+sz=size(BW);
+maxd=sqrt(sz(1)^2+sz(2)^2+sz(3)^2);
+rho=-maxd:maxd;
+h=zeros(size(theta,2),size(phi,2),size(rho,2));
+
+
+
+end
 

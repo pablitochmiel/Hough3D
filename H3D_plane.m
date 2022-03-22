63d85fd33ed06e1d8fa20bf82f59dc74726ffa26
pablito <pawcio27610@wp.pl>
2022-03-22 22:50:17 +0100

    works

--- H3D_plane.m
+++ H3D_plane.m
@@ -1,6 +1,7 @@
 close all;clc;clear;
 
-data=makeDataSimple();
+%data=makeDataSimple();
+data=makeData1();
 
 % cos=data==1;
 % [x,y,z] = data;
@@ -12,6 +13,7 @@ data=makeDataSimple();
 % xlim([0 5])
 % ylim([0 10])
 % zlim([0 16])
+
 [X,Y,Z]=convertTo3Vec(data);
 sz=size(data);
 scatter3(X,Y,Z,'r.');
@@ -25,30 +27,66 @@ zlabel('z')
 
 [H, Theta, Phi, Rho] = hough3Dplane(data);
 
+mx=max(max(max(H)));% find the max score location
+%[aatheta, aaphi, aarho]=find(H==mx);
+%fileID = fopen('plik.txt','w');
+%petla
+sz3=size(H);
+for i=1:sz3(1)
+    for j=1:sz3(2)-1
+        for k=1:sz3(3)
+            if H(i,j,k) == mx
+                %fprintf( fileID, '%d %d %d\r\n', Theta(i), Phi(j), Rho(k));
+                disp(string(Theta(i))+' '+string(Phi(j))+' '+string(Rho(k)));
+            end
+        end
+    end
+end
+for k=1:sz3(3)
+    if H(1,sz3(2),k) == mx
+        %fprintf( fileID, '%d %d %d\r\n', Theta(1), Phi(sz3(2)), Rho(k));
+        disp(string(Theta(1))+' '+string(Phi(sz3(2)))+' '+string(Rho(k)));
+    end
+end
+
+%fclose(fileID);
+% for i=1:sz3(3)
+%     rescale(H(:,:,i));
+% end
+%figure;
+% implay(H);
 
 function[h, theta, phi, rho] = hough3Dplane(BW)
-fun=@(x,y,z,theta,phi) x.*cosd(theta).*cosd(phi) + y.*sind(theta).*cosd(phi) + z.*sind(phi);
-theta=-90:89;
-phi=-90:89;
+fun=@(x,y,z,theta,phi) round(x.*cosd(theta).*cosd(phi) + y.*sind(theta).*cosd(phi) + z.*sind(phi),1); 
+% theta=0:179;
+% phi=-90:89;
+theta=0:90;
+phi=0:90;
 sz=size(BW);
-maxd=sqrt(sz(1)^2+sz(2)^2+sz(3)^2);
-rho=-maxd:maxd;
+maxd=round(sqrt(sz(1)^2+sz(2)^2+sz(3)^2))+1;
+%rho=-maxd:0.1:maxd;
+rho=0:0.1:maxd;
 h=zeros(size(theta,2),size(phi,2),size(rho,2));
 
 for i = 1:sz(1)
     for j = 1:sz(2)
        for k = 1:sz(3)
            if(BW(i,j,k)==1)
-               figure;
-               [xx,yy]= meshgrid(theta,phi);
-               zz=fun(i,j,k,xx,yy);
-               mesh(xx,yy,zz)
-               xlabel('\theta'), ylabel('\phi'),zlabel('/rho');
+%              figure;
+               [yy,xx]= meshgrid(theta,phi);
+               zz=fun(i,j,k,yy,xx);
+%                mesh(yy,xx,zz)
+%                xlabel('\theta'), ylabel('\phi'),zlabel('\rho');
+               sz2=size(zz);
+               for l=1:sz2(2)
+                   for m=1:sz2(1)
+                       h(l,m,rho==zz(m,l))=h(l,m,rho==zz(m,l))+1;
+                   end
+               end
            end
        end
     end
 end
-
 
 end
 
--- temp.m
+++ temp.m
@@ -4,10 +4,10 @@ close all;clc;clear;
 % BW = edge(I,'canny');
 % [H,T,R] = hough(BW,'RhoResolution',0.5,'Theta',-90:0.5:89);
 BW=zeros(50,50);
-BW(10,10)=1;
-BW(40,40)=1;
-% BW(10,10:40)=1;
-% BW(40,10:40)=1;
+BW(10:40,10)=1;
+BW(10:40,40)=1;
+BW(10,10:40)=1;
+BW(40,10:40)=1;
 
 [H,T,R] = hough(BW);
 subplot(2,1,1);
@@ -17,8 +17,9 @@ hold on;
 mx=max(max(H));% find the max score location
 [y,x]=find(H==mx);
 subplot(2,1,2);
-imshow(imadjust(rescale(H)),'XData',T,'YData',R,...
-      'InitialMagnification','fit');
+% imshow(imadjust(rescale(H)),'XData',T,'YData',R,...
+%       'InitialMagnification','fit');
+imshow(imadjust(rescale(H)));
 title('Hough transform of gantrycrane.png');
 xlabel('\theta'), ylabel('\rho');
 axis on, axis normal, hold on;

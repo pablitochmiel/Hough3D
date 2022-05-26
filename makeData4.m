b2c56d3e46093274b47da95dc209fe1f598c0642
pablito <pawcio27610@wp.pl>
2022-05-26 09:42:20 +0200

    add files

---
+++ makeData4.m
@@ -0,0 +1,16 @@
+function [data] = makeData4()
+data=zeros(50,100,50);
+sz=size(data);
+
+for i = 1:sz(1)
+    for j = 1:sz(2)/2
+       for k = 10:40
+           a=i-25;
+           b=j-25;
+           radius=a*a+b*b;
+           if((radius>390 && radius < 410)||(k==10 &&  radius < 410)||(k==40 &&  radius < 410))
+               data(i,j,k)=1;
+           end
+       end
+    end
+end
